9737caca698295385937585d0ec76c718bbc154f
camelactive <79437316+camelactive@users.noreply.github.com>
2022-01-15 15:41:31 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
-let pidori = ["Никита", "никита", "НИКИТА", "Nikita", "nikita", "NIKITA"]
+let pidori = ["Никита", "никита", "НИКИТА", "Nikita", "nikita", "NIKITA"];
+let pedofil = ["Андрей", "андрей", "АНДРЕЙ", "Andrey", "andrey", "ANDREY"];
 inputNameHandler = e => {
     // console.log(e.value);
     let name = e.value;
@@ -9,6 +10,16 @@ inputNameHandler = e => {
             let greetings_content_write = document.querySelector(".greetings_content_write");
             greetings_content_write.style.fontSize = "54px";
             greetings_content_write.insertAdjacentText("afterbegin", `Приветствую тебя сэр ${e.value}`)
+
+        }
+    };
+    for (let i = 0; i < pedofil.length; i++) {
+        if (e.value == pedofil[i]) {
+            e.value = "вонючка" + " " + name;
+            let x = (`Приветствую тебя любитель детей ${e.value}`)
+            let greetings_content_write = document.querySelector(".greetings_content_write");
+            greetings_content_write.style.fontSize = "54px";
+            greetings_content_write.insertAdjacentText("afterbegin", `Приветствую тебя педофил ${e.value}`)
 
         }
     }
